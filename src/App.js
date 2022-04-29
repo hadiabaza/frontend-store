ca4d065e7f2a4d4e1e63cddd9355c9c23ea33438
Hadi Abaza <hadi@Hadis-MBP-14.lan>
2022-04-29 00:54:05 -0400

    add function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useCart } from './useCart';
 
-const marketPlace = [{price: $ +'10',productName: 'Dark side of the moon - Pink Floyd', id: 1, img: 'https://cdn.lorem.space/images/album/.cache/150x150/dark-side-of-the-moon_pink-floyd.jpg'},{price: $ +'10',productName: 'Take care - Drake', id:2, img: 'https://cdn.lorem.space/images/album/.cache/150x150/take-care_drake.jpg'},{price: $ +'10',productName: 'Nevermind - Nirvana', id:3, img: 'https://cdn.lorem.space/images/album/.cache/150x150/nevermind_nirvana.jpg'}, {price: $ +'10',productName: 'I wanna thank me - Snoop Dogg',id: 4, img: 'https://cdn.lorem.space/images/album/.cache/150x150/i-wanna-thank-me_snoop-dogg.jpg'}] 
+const marketPlace = [{price:  10,productName: 'Dark side of the moon - Pink Floyd', id: 1, img: 'https://cdn.lorem.space/images/album/.cache/150x150/dark-side-of-the-moon_pink-floyd.jpg'},{price:  10,productName: 'Take care - Drake', id:2, img: 'https://cdn.lorem.space/images/album/.cache/150x150/take-care_drake.jpg'},{price:  10,productName: 'Nevermind - Nirvana', id:3, img: 'https://cdn.lorem.space/images/album/.cache/150x150/nevermind_nirvana.jpg'}, {price:  10,productName: 'I wanna thank me - Snoop Dogg',id: 4, img: 'https://cdn.lorem.space/images/album/.cache/150x150/i-wanna-thank-me_snoop-dogg.jpg'}] 
 
 
 function App() {
@@ -11,13 +11,25 @@ function App() {
     padding: '20px'
   }
 
+  const handleAdd = (item) => {
+    console.log(item)
+    return cart.addItem({productName: item.productName, id: item.id, price: item.price, img: item.img})
+  }
+
   const renderedItems = marketPlace.map(item => {
-    return (<img 
-    onClick={cart.addItem(productName: item.productName, id: item.id, price: item.price)} 
-    style={imgRender}
-    key={item.id} 
-    src={item.img} 
-    alt={item.id}></img>)
+    return (
+    <div>
+      <img 
+      onClick={() => handleAdd(item)} 
+      style={imgRender}
+      key={item.id} 
+      src={item.img} 
+      alt={item.id}>
+      </img>
+      <div>{item.productName}</div>
+      <div>{item.price}</div>
+    </div>
+    )
   })
   
   return (
